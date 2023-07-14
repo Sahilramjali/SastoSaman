c99720cf696b76a67782ecd25de62aef9c8933e6
Sahilramjali <sahil.ramjali13@gmail.com>
2023-07-14 13:38:17 +0545

    create user model

---
+++ server/Models/user.js
@@ -0,0 +1,25 @@
+import {Schema,model} from 'mongoose';
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required:[true, 'is required field'],
+    },
+    email:{
+        type:String,
+        required:[true,'is required field'],
+        unique:true,
+        lowercase:true,
+    },
+    password:{
+        type:String,
+        required:[true,"is required field"],
+        select:false
+    }
+},
+{timestamps:true}
+);
+
+const User=model('user',userSchema);
+
+export default User;
