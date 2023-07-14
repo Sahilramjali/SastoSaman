47f8c5f27974cc4e41f4fe45dd723cd45e417537
Sahilramjali <sahil.ramjali13@gmail.com>
2023-07-14 13:34:14 +0545

    database connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 
 import express from 'express';
 
-
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app=express();
 const PORT=5000;
-
-
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("data base connected")
+}).catch(()=>{
+    console.log("data base connection error");
+})
 app.listen(PORT,()=>{
     console.log("Server is running on port "+PORT);
 })
