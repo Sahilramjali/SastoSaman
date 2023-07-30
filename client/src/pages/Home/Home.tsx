dae2b8d0405b0a6244696e4defe72a17206add14
Sahilramjali <sahil.ramjali13@gmail.com>
2023-07-30 09:37:33 +0545

    cart

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,13 +3,41 @@ import Carousel from "../../components/Carousel"
 import CategoryComponent from "../../components/Category/CategoryItemsCarousel"
 import Footer from "../../components/Footer/Footer"
 
-import { useEffect } from "react"
+import { useCallback, useEffect } from "react"
+import axios from "axios"
+import { useAppDispatch } from "../../hooks/useAppDispatch"
+import { addToCart } from "../../redux/cartSlice"
+import { useCookies } from "react-cookie"
 
 const Home = () => {
-
+const dispatch=useAppDispatch();
+const[cookies,,]=useCookies(['user']);
   useEffect(()=>{
     document.title=`SastoSaman: Home`
-  })
+  });
+
+  const fetchCartItems = useCallback(async () => {
+    await axios
+      .get("http://localhost:5000/api/cart/getCart", {
+        headers: {
+          authorization: `beare ${cookies?.user?.token}`,
+        },
+      })
+      .then((res) => {
+       
+        dispatch(addToCart(res.data.cartItems));
+      })
+      .catch((err) => {
+        console.log(err);
+       
+      });
+  }, [cookies?.user?.token, dispatch]);
+
+  useEffect(() => {
+    if (cookies?.user?.token) {
+      fetchCartItems();
+    }
+  }, []);
   return (
     <>
      <Carousel/>
